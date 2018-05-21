065da0a898032055ed14d63def9b9e7b994a93e2
KBClayton <clayton.kevinb@gmail.com>
2018-05-21 18:21:43 -0500

    errors

--- maps.js
+++ maps.js
@@ -82,8 +82,8 @@ $(document).ready(function() {
     }   
     if(elevationURL!==" "){
     
-        var elevator = new google.maps.ElevationService;
-        elevator.LocationElevationRequest(30.2672, -97.7431);
+     //   var elevator = new google.maps.ElevationService;
+     //   elevator.LocationElevationRequest(30.2672, -97.7431);
         $.ajax({
             url: elevationURL,
             method: "GET",
